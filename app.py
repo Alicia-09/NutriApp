756f15fbcfeb781d870d336c7df01fe32e062622
Alicia-09 <24308060610613@cetis61.edu.mx>
2025-11-05 11:11:18 -0500

    Add files via upload
    cambios

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template,request,flash,url_for,redirect
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("inicio.html")
+
+@app.route("/inicio")
+def index():
+    return render_template("inicio.html")
+
+@app.route("/crearCuenta" , methods = ["GET", "POST"])
+def crearCuenta():
+
+    error=None
+    if request.method == "POST":
+        Nombre = request.form["Nombre"]
+        Apellido = request.form["Apellido"]
+        Fecha = request.form["Fecha"]
+        Genero = request.form["Genero"]
+        Email = request.form["Email"]
+        Contra = request.form["Contra"]
+        ContraConfirm = request.form["ContraConfirm"]
+    
+        if Contra != ContraConfirm:
+            error = "La contraseña no coincide"
+            if error != None:
+                flash(error)
+            return render_template("crearCuenta.html")
+            
+        flash(f"Registro exitoso para el usuario:{Nombre} {Apellido}")
+        return redirect (url_for ("inicio.html"))
+    
+    return render_template("crearCuenta.html")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
